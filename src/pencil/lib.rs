dd1ff2145f7adda4615894563fcc7733e7495d2f
Shipeng Feng <fsp261@gmail.com>
2014-11-06 17:01:17 +0800

    Added public api comment

--- src/pencil/lib.rs
+++ src/pencil/lib.rs
@@ -1,6 +1,8 @@
 // A microframework for Rust.
 // Copyright (c) 2014 by Shipeng Feng.
 // Licensed under the BSD License, see LICENSE for more details.
+
+/* public api */
 pub use app::Pencil;
 
 mod app;
