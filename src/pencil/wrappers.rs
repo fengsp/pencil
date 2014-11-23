5e2fe0272b9c6b418e635ace4cf3348e72a47616
Shipeng Feng <fsp261@gmail.com>
2014-11-23 19:32:44 +0800

    Added response content length

--- src/pencil/wrappers.rs
+++ src/pencil/wrappers.rs
@@ -4,7 +4,7 @@
 
 use core;
 use std::iter;
-use std::ascii::OwnedAsciiExt;
+use std::ascii::AsciiExt;
 
 use http::status;
 
@@ -34,11 +34,11 @@ impl Headers {
 
     /// Return a reference to the value corresponding to the header key.
     pub fn get(&self, key: String) -> Option<&String> {
-        let ikey = key.into_ascii_lower();
+        let ikey = key.to_ascii_lower();
         for ref kvpair in self.list.iter() {
             let k = kvpair.ref0();
             let v = kvpair.ref1();
-            if k.clone().into_ascii_lower() == ikey {
+            if k.to_ascii_lower() == ikey {
                 return Some(v)
             }
         }
@@ -48,12 +48,12 @@ impl Headers {
     /// Return a list of all the references to the values for a given key.
     /// If that key is not in the headers, the return value will be an empty vector.
     pub fn get_all(&self, key: String) -> Vec<&String> {
-        let ikey = key.into_ascii_lower();
+        let ikey = key.to_ascii_lower();
         let mut result = Vec::new();
         for ref kvpair in self.list.iter() {
             let k = kvpair.ref0();
             let v = kvpair.ref1();
-            if k.clone().into_ascii_lower() == ikey {
+            if k.to_ascii_lower() == ikey {
                 result.push(v);
             }
         }
@@ -87,13 +87,13 @@ impl Headers {
     /// Removes a key from the headers, returning the first value at the key
     /// if the key was previously in the headers.
     pub fn remove(&mut self, key: String) -> Option<String> {
-        let ikey = key.into_ascii_lower();
+        let ikey = key.to_ascii_lower();
         let mut rv: Option<String> = None;
         let mut newlist = Vec::new();
         for kvpair in self.list.iter() {
             let k = kvpair.ref0();
             let v = kvpair.ref1();
-            if k.clone().into_ascii_lower() != ikey {
+            if k.to_ascii_lower() != ikey {
                 newlist.push((k.clone(), v.clone()));
             } else if rv != None {
                 rv = Some(v.clone());
@@ -107,13 +107,13 @@ impl Headers {
     /// appears at the end of the list if there was no entry or replaces the old one.
     /// TODO: _option_header_vkw
     pub fn set(&mut self, key: String, value: String) {
-        let ikey = key.clone().into_ascii_lower();
+        let ikey = key.to_ascii_lower();
         let mut key_existed = false;
         let mut newlist = Vec::new();
         for kvpair in self.list.iter() {
             let k = kvpair.ref0();
             let v = kvpair.ref1();
-            if k.clone().into_ascii_lower() != ikey {
+            if k.to_ascii_lower() != ikey {
                 newlist.push((k.clone(), v.clone()));
             } else if !key_existed {
                 newlist.push((key.clone(), value.clone()));
@@ -141,7 +141,7 @@ impl Headers {
 /// Response type.  It is just one container with a couple of parameters
 /// (headers, body, status code etc).
 pub struct Response {
-    pub status: status::Status,
+    status: status::Status,
     pub headers: Headers,
     pub body: String,
 }
@@ -149,11 +149,16 @@ pub struct Response {
 impl Response {
     /// Create a `Response`.
     pub fn new(body: String) -> Response {
-        Response {
+        let mut response = Response {
             status: status::Ok,
             headers: Headers::new(None),
             body: body,
-        }
+        };
+        let content_type = String::from_str("text/plain; charset=utf-8");
+        let content_length = response.body.len().to_string();
+        response.headers.set(String::from_str("Content-Type"), content_type);
+        response.headers.set(String::from_str("Content-Length"), content_length);
+        return response;
     }
 
     pub fn get_status_code(&self) -> u16 {
@@ -173,5 +178,41 @@ impl Response {
 
     pub fn set_status(&mut self, status: status::Status) {
         self.status = status;
+    }
+
+    /// Sets a new string as response body.  The content length is set
+    /// automatically.
+    pub fn set_data(&mut self, value: String) {
+        self.body = value;
+        let content_length = self.body.len().to_string();
+        self.headers.set(String::from_str("Content-Length"), content_length);
+    }
+
+    /// Returns the response content type if available.
+    pub fn get_content_type(&self) -> Option<String> {
+        let rv = self.headers.get(String::from_str("Content-Type"));
+        match rv {
+            Some(content_type) => content_type.clone(),
+            None => None,
+        }
+    }
+
+    /// Set response content type.
+    pub fn set_content_type(&mut self, value: String) {
+        self.headers.set(String::from_str("Content-Type"), value);
+    }
+
+    /// Returns the response content length if available.
+    pub fn get_content_length(&self) -> Option<uint> {
+        let rv = self.headers.get(String::from_str("Content-Length"));
+        match rv {
+            Some(content_length) => from_str(content_length.as_slice()),
+            None => None,
+        }
+    }
+
+    /// Set content length.
+    pub fn set_content_length(&mut self, value: uint) {
+        self.headers.set(String::from_str("Content-Length"), value.to_string());
     }
 }
