41bd67bacc5c78747d2fb09722f0f62febf010ff
Adam Faur <adam.faur@gmail.com>
2016-07-01 14:47:52 -0500

    Add ability to set thread count.

--- src/serving.rs
+++ src/serving.rs
@@ -1,5 +1,8 @@
 //! This module implements the http server support for our application.
 
+use std::env;
+use std::str::FromStr;
+
 use std::net::ToSocketAddrs;
 
 use hyper::server::Server;
@@ -9,6 +12,9 @@ use app::Pencil;
 
 /// Run the `Pencil` application.
 pub fn run_server<A: ToSocketAddrs>(application: Pencil, addr: A) {
+    let threads_str = env::var("THREADS").unwrap_or(String::new());
+    let threads = FromStr::from_str(&threads_str).unwrap_or(10) as usize;
+
     let server = Server::http(addr).unwrap();
-    let _guard = server.handle(application).unwrap();
+    let _guard = server.handle_threads(application,threads).unwrap();
 }
